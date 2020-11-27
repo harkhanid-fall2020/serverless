a4677cfba999b74b9f33aad01a6db6d91bfc2034
Dharmik Harkhani <harkhani.d@northeastern.edu>
2020-11-26 19:40:56 -0500

    added Appspecyml

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const add = ({num1 , num2}) =>{
-    return num1 + num2;
-}
+const aws = require('aws-sdk');
+const ses = new aws.SES({ region: "us-east-1" });
 
 const main = async event =>{
-    console.log("Event :", event);
-    return add(event);
+    //getting Message from SNS Topic
+    //let message = event.Records[0].Sns.Message;
+    //console.log(message);
+
+    
+    //sending Mail
+    
+    const params = {
+    Destination: {
+      ToAddresses: ["dharmikharkhani12345@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "Test" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    Source: "demo@dev.dharmikharkhani.me",
+  };
+ 
+  return ses.sendEmail(params).promise()
+    
 }
 
 exports.handler = main;
